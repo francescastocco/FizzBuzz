5dd95c6fe31e0ce3f986acde607aaea2a8c9cbf3
Ken Allan <Ken.Allan@softwire.com>
2021-05-24 14:57:24 +0100

    part4 completed

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,4 +5,14 @@
 // if they do both then return fizzbuzz
 // use console.log to print to the console.
 
-console.log("Hello World");
+for (let i=1; i<101; i++) {
+    if ((i%3 === 0) && (i%5 === 0)) {
+        console.log("FizzBuzz");
+    } else if (i%3 === 0) {
+        console.log("Fizz");
+    } else if (i%5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
