99d3e586185d687113e2522cc331e15f2474165b
Ken Allan <Ken.Allan@softwire.com>
2021-05-24 14:21:16 +0100

    initial commit

---
+++ FizzBuzz.js
@@ -0,0 +1,9 @@
+
+// for loop to increment through 
+// check if they are multiples of 3 and return fizz
+// check if they are multiples of 5 and return buzz
+// if they do both then return fizzbuzz
+// use console.log to print to the console.
+
+
+suwanna
